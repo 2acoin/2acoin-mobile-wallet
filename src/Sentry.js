5dd69feaf11252d35fbc2aeef7872f35a0b9fcc8
2ACoin Miner <miner@2acoin.org>
2019-07-02 13:31:48 -0500

    Update jetifier and wallet-backend    - modify sentry loggin    - modify database json check    - 5ed59a2

--- src/Sentry.js
+++ src/Sentry.js
@@ -4,6 +4,8 @@
 
 import { Sentry } from 'react-native-sentry';
 
+import * as _ from 'lodash';
+
 import Config from './Config';
 
 /* Manually comparing to TurtleCoin to try and prevent getting errors reported
@@ -12,6 +14,12 @@ import Config from './Config';
 const sentryIsEnabled = !__DEV__ && Config.coinName === '2ACoin';
 
 export function reportCaughtException(err) {
+    /* Sentry doesn't properly report arbitary objects. Convert to string if
+       it ain't a string or an error. */
+    if (!_.isString(err) && !(err instanceof Error)) {
+        err = JSON.stringify(err, null, 4);
+    }
+
     if (sentryIsEnabled) {
         try {
             Sentry.captureException(err);
@@ -25,5 +33,8 @@ export function initSentry() {
         /* CHANGE THIS IF YOU ARE FORKING! */
         Sentry.config('https://14dd359d38314269b1be5b2c164a478e@sentry.io/1482559').install();
         Sentry.setVersion(Config.appVersion);
+        Sentry.setDataCallback((event) => {
+            console.log(event);
+        });
     }
 }
