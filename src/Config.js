f2fe398f87f76fd51eadde49ba0ad5adbe65c064
zpalmtree <22151537+zpalmtree@users.noreply.github.com>
2019-07-19 06:19:38 +0100

    Fix OOM at certain block ranges

--- src/Config.js
+++ src/Config.js
@@ -150,7 +150,7 @@ const Config = {
     /**
      * Amount of blocks to request from the daemon at once
      */
-    blocksPerDaemonRequest: 100,
+    blocksPerDaemonRequest: 20,
 
     /**
      * Unix timestamp of the time your chain was launched.
