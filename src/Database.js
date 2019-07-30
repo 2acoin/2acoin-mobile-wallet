75b7bea4fd2bd5472bed85ad079b6058ac8b7776
zpalmtree <22151537+zpalmtree@users.noreply.github.com>
2019-07-30 07:42:57 +0100

    Generic authentication method - eb028e6

--- src/Database.js
+++ src/Database.js
@@ -203,7 +203,7 @@ export async function savePreferencesToDatabase(preferences) {
                 preferences.scanCoinbaseTransactions ? 1 : 0,
                 preferences.limitData ? 1 : 0,
                 preferences.theme,
-                preferences.pinConfirmation ? 1 : 0,
+                preferences.authConfirmation ? 1 : 0,
                 preferences.autoOptimize ? 1 : 0,
             ]
         );
@@ -235,7 +235,7 @@ export async function loadPreferencesFromDatabase() {
             scanCoinbaseTransactions: item.scancoinbasetransactions === 1,
             limitData: item.limitdata === 1,
             theme: item.theme,
-            pinConfirmation: item.pinconfirmation === 1,
+            authConfirmation: item.pinconfirmation === 1,
             autoOptimize: item.autooptimize === 1,
         }
     }
