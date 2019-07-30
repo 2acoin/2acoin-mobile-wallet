029957321190658cfc3a107a59bcf76111e42442
zpalmtree <22151537+zpalmtree@users.noreply.github.com>
2019-07-30 06:51:08 +0100

    Add touch ID / face ID wallet unlocking - 6ffb0c5

--- src/MainScreen.js
+++ src/MainScreen.js
@@ -468,7 +468,7 @@ async function backgroundSave() {
     Globals.logger.addLogMessage('Saving wallet...');
 
     try {
-        await saveToDatabase(Globals.wallet, Globals.pinCode);
+        await saveToDatabase(Globals.wallet);
         Globals.logger.addLogMessage('Save complete.');
     } catch (err) {
         reportCaughtException(err);
